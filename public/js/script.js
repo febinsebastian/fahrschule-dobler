d31cf8fcbe6036c45526904115dfeb15b1267c4b
Febin Sebastian <febinsebastian@Febins-MacBook-Air.local>
2019-04-06 06:45:56 +0200

    new changes

--- public/js/script.js
+++ public/js/script.js
@@ -1,40 +1,50 @@
-$(function() {
-    $("#submitBtn").click(function() {
+$(function () {
+    $("#submitBtn").click(function () {
         var name = $("#studentName").val();
         var date = $("#myModal").attr("date");
         var startTime = $("#startTime").val();
         var endTime = $("#endTime").val();
-        var event =  {
-            id     : new Date().getTime(),
-            title  : name,
-            start  : date+"T"+startTime,
-            end    : date+"T"+endTime,
-            allDay : false
+        var event = {
+            id: new Date().getTime(),
+            title: name,
+            start: date + "T" + startTime,
+            end: date + "T" + endTime,
+            allDay: false
         };
         var eventsArray = [];
-        if(localStorage.getItem("eventsArray"))
+        if (localStorage.getItem("eventsArray"))
             eventsArray = JSON.parse(localStorage.getItem("eventsArray"));
         eventsArray.push(event);
-        localStorage.setItem("eventsArray",JSON.stringify(eventsArray));
-        $('#calendar').fullCalendar("renderEvent",event);
+        localStorage.setItem("eventsArray", JSON.stringify(eventsArray));
+        $('#calendar').fullCalendar("renderEvent", event);
         $("#myModal").modal('hide');
-      });
+    });
 
-      $("#deleteBtn").click(function(){
-          if(confirm("Are you want to delete ?")){
+    $("#deleteBtn").click(function () {
+        if (confirm("Are you want to delete ?")) {
             var eventsArray = [];
-            if(localStorage.getItem("eventsArray"))
+            if (localStorage.getItem("eventsArray"))
                 eventsArray = JSON.parse(localStorage.getItem("eventsArray"));
             var id = $("#myModal").attr("eventId");
-            for(var i = 0; i<eventsArray.length; i++){
-                if(eventsArray[i]['id'] == id){
+            for (var i = 0; i < eventsArray.length; i++) {
+                if (eventsArray[i]['id'] == id) {
                     eventsArray.splice(i, 1);
                     break;
                 }
             };
-            $('#calendar').fullCalendar("removeEvents",id);
-            localStorage.setItem("eventsArray",JSON.stringify(eventsArray));
+            $('#calendar').fullCalendar("removeEvents", id);
+            localStorage.setItem("eventsArray", JSON.stringify(eventsArray));
             $("#myModal").modal('hide');
-          }
-      });
+        }
+    });
+    $("scheduleForm").click(function () {
+        var days = ['monday','tuesday','wednesday','thursday','friday','saturday'];
+        
+    });
+    $("#addStudent").submit(function(){
+        function getRandomInt(max) {
+            return Math.floor(Math.random() * Math.floor(max));
+        }
+        console.log(getRandomInt(100000));
+    });
 });
