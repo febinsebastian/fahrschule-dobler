c60a5d4f5595a71ad2549a5f8e42bdbf33449fe1
Febin Sebastian <febinsebastian@Febins-MacBook-Air.local>
2019-05-02 21:15:16 +0200

    login page

--- public/js/addStudent.js
+++ public/js/addStudent.js
@@ -1,6 +1,6 @@
 $(function () {
     const URL = 'https://febindrivingschool.000webhostapp.com/febin_driving_school_teacher/index.php/drivingSchool';
-    var student = {"status":true,"data":[{"id":"2","name":"Jerin","email":"jerinjoshy1@gmail.com","mobile":"9567376700"},{"id":"3","name":"Febin","email":"febin@gmail.com","mobile":"123456789"},{"id":"4","name":"dfwd","email":"febinchemperi@gmail.com","mobile":"9916799370"},{"id":"5","name":"ewew","email":"sd@www.dd","mobile":"2323232332"},{"id":"6","name":"sd","email":"febines12@gmail.com","mobile":"1234567890"},{"id":"7","name":"febnin","email":"fsdd@ksklf.jf","mobile":"9848574565"},{"id":"8","name":"sd","email":"febinchemperi@gmail.com","mobile":"1111111111"}]};
+    //var student = {"status":true,"data":[{"id":"2","name":"Jerin","email":"jerinjoshy1@gmail.com","mobile":"9567376700"},{"id":"3","name":"Febin","email":"febin@gmail.com","mobile":"123456789"},{"id":"4","name":"dfwd","email":"febinchemperi@gmail.com","mobile":"9916799370"},{"id":"5","name":"ewew","email":"sd@www.dd","mobile":"2323232332"},{"id":"6","name":"sd","email":"febines12@gmail.com","mobile":"1234567890"},{"id":"7","name":"febnin","email":"fsdd@ksklf.jf","mobile":"9848574565"},{"id":"8","name":"sd","email":"febinchemperi@gmail.com","mobile":"1111111111"}]};
     updateList();
     
     //Onclick on the Add Student Button following function will execute
@@ -63,6 +63,7 @@ $(function () {
             },
             success: function (data) { 
                 console.log(data);
+                updateList(data);
             },
             error: function (jqXHR, textStatus, errorThrown) { 
                 console.log(jqXHR);
@@ -71,12 +72,11 @@ $(function () {
             },
             complete: function(){
                 loader("stop");
-                updateList();
             }
         });
     }
 
-    function updateList(){
+    function updateList(student){
         var student_list = student.data;
         var student_list_table = '';
         for(var i=0;i<student_list.length;i++){
